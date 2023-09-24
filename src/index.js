5a984564d266bb178131d2d99c36a17d59ca2cd6
maershaa <valeria.work2022@gmail.com>
2023-09-24 12:41:20 +0300

    infifnitt scroll update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Импорт необходимых библиотек и стилей
 import throttle from 'lodash.throttle'; // Lodash Throttle - библиотека для создания функций с задержкой выполнения.
 import Notiflix from 'notiflix'; // Notiflix - библиотека для создания уведомлений в веб-приложениях.
 import 'notiflix/dist/notiflix-3.2.6.min.css';
@@ -6,18 +7,22 @@ import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Импорт необходимых функций и данных из других модулей
 import { refs, totalPages } from './js/refs';
 import { createMarkup } from './js/markup';
 import { getPhotos } from './js/query_api';
 
 import { scrollPage } from './js/scroll.js';
 
+// Инициализация переменных
 let currentPage = 1;
 let perPage = 40; // Количество элементов (изображений) на странице.
 
+// Скрываем кнопки "Load more" и "Scroll" по умолчанию
 refs.loadMoreButton.classList.add('hidden');
 refs.scrollButton.classList.add('hidden');
 
+// Инициализация Lightbox для просмотра изображений
 const lightbox = new SimpleLightbox('.gallery a', {
   sourceAttr: 'href',
   captionsData: 'alt',
@@ -45,32 +50,34 @@ async function onFormSubmit(evt) {
     return; // Возвращаемся из функции, так как поле пустое
   } else {
     try {
+      // Запрашиваем данные изображений по введенному запросу и странице
       const photoArr = await getPhotos(userInput, 1);
-      // Получаем данные изображений и сохраняем в переменные
 
+      // Если запрос не пустой, очищаем галерею
       if (userInput !== '') {
         clearGallery(); // Очищаем галерею
       }
 
+      // Если найдены изображения, то создаем разметку и добавляем ее в галерею
       if (parseInt(photoArr.totalHits) > 0) {
-        // Если найдены изображения, то создаем разметку и добавляем ее в галерею
         const markup = createMarkup(photoArr.hits); // Создаем HTML-разметку из данных hits
         refs.gallery.innerHTML += markup; // Добавляем разметку в галерею
 
         // Запускаем наблюдение для infinityscroll, если все доступные фотографии загружены
         observer.observe(target);
 
+        // Обновляем Lightbox, чтобы учесть новые элементы
         lightbox.refresh();
 
         // Выводим информацию о количестве найденных изображений
         Notiflix.Notify.info(`Hooray! We found ${photoArr.totalHits} images.`);
 
+        // Если есть еще изображения для загрузки, показываем кнопку "Scroll"
         if (photoArr.hits !== photoArr.totalHits) {
-          // Если есть еще изображения для загрузки, то показываем кнопку "Load more"
-          // refs.loadMoreButton.classList.remove('hidden'); // Показываем кнопку "Load more"
           refs.scrollButton.classList.remove('hidden'); // Показываем кнопку прокрутки
         }
 
+        // Если запрос пустой, сбрасываем текущую страницу на 1
         if (userInput === '') {
           currentPage = 1;
         }
@@ -82,32 +89,34 @@ async function onFormSubmit(evt) {
         if (currentPage >= totalPages) {
           // Останавливаем наблюдение для infinityscroll, если все доступные фотографии загружены
           observer.unobserve(target);
-
-          // refs.loadMoreButton.classList.add('hidden'); // убираем кнопку "Load more"
         }
 
         // Возвращаем информацию о текущей странице, общем количестве страниц и массиве изображений
         return { currentPage, totalPages, hits: photoArr.hits };
       } else {
-        // Если фотографий для загрузки больше нет, скрываем кнопку
-
+        // Если фотографий для загрузки больше нет, скрываем кнопку "Load more"
         refs.loadMoreButton.classList.add('hidden');
 
+        // Выводим уведомление о том, что больше изображений нет
         Notiflix.Notify.info('No more images to load.');
       }
     } catch (error) {
-      // В случае ошибки показываем уведомление
+      // В случае ошибки выводим уведомление
       console.error(error);
       Notiflix.Notify.failure('No photos found for your query');
     }
   }
 }
 
+// Функция для очистки галереи
 function clearGallery() {
   while (refs.gallery.firstChild) {
     refs.gallery.removeChild(refs.gallery.firstChild);
   }
 }
+
+// Добавляем слушателя события для кнопки "Scroll"
+refs.scrollButton.addEventListener('click', scrollPage);
 
 // ====================================================================================
 // =========  Реализация подгрузки фото через нажатие кнопки LoadMore =================
@@ -144,8 +153,6 @@ function clearGallery() {
 //   lightbox.refresh();
 // }
 
-refs.scrollButton.addEventListener('click', scrollPage);
-
 // ====================================================================================
 // =========  Реализация подгрузки фото через infinity scroll ========================
 // ====================================================================================
@@ -162,18 +169,30 @@ let observer = new IntersectionObserver(onLoad, options);
 
 async function onLoad(entries, observer) {
   entries.forEach(async entry => {
+    // Проверяем, видим ли элемент
     if (entry.isIntersecting) {
+      // Увеличиваем текущую страницу
       currentPage += 1;
-      try {
-        const { hits } = await getPhotos(userInput, currentPage); // Вызываем функцию для загрузки данных
-        // Обновляем галерею с новыми данными
-        const markup = createMarkup(hits);
-        refs.gallery.innerHTML += markup;
-        // Уничтожаем и повторно инициализируем Lightbox
-        lightbox.refresh();
-      } catch (error) {
-        console.error(error);
-        Notiflix.Notify.failure('Failed to load more photos');
+
+      // Проверяем, есть ли еще страницы для загрузки
+      if (currentPage < totalPages) {
+        try {
+          // Загружаем данные для следующей страницы
+          const { hits } = await getPhotos(userInput, currentPage);
+
+          // Создаем HTML-разметку на основе загруженных данных
+          const markup = createMarkup(hits);
+
+          // Добавляем разметку в галерею
+          refs.gallery.innerHTML += markup;
+
+          // Обновляем Lightbox, чтобы учесть новые элементы
+          lightbox.refresh();
+        } catch (error) {
+          // В случае ошибки выводим уведомление
+          console.error(error);
+          Notiflix.Notify.failure('Failed to load more photos');
+        }
       }
     }
   });
